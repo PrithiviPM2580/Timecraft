51c27c38108de2fb6dc3c6d52f14df6f54bbfb2a
Prithivi <prithivi@users.noreply.github.com>
2026-02-08 00:18:03 +0545

    Add database initialization function with error handling and logging

---
+++ backend/src/database/database.ts
@@ -0,0 +1,17 @@
+import { AppDataSource } from "../config/database.config.js";
+import logger from "../utils/logger.js";
+
+export const initilizeDatabase = async () => {
+  try {
+    await AppDataSource.initialize();
+    logger.info("Database connection initialized successfully", {
+      label: "Database Initialization",
+    });
+  } catch (error) {
+    logger.error("Error initializing database connection:", {
+      label: "Database Initialization",
+      error: error instanceof Error ? error.message : String(error),
+    });
+    process.exit(1);
+  }
+};
