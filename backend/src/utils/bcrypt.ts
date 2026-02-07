6384608d76dc7e4ae673e4c3e715b6d1165586a7
Prithivi <prithivi@users.noreply.github.com>
2026-02-08 02:07:58 +0545

    Add bcrypt utility functions for hashing and comparing values

---
+++ backend/src/utils/bcrypt.ts
@@ -0,0 +1,15 @@
+import bcrypt from "bcrypt";
+
+export const hashValue = async (
+  value: string,
+  saltRound: number = 10,
+): Promise<string> => {
+  return await bcrypt.hash(value, saltRound);
+};
+
+export const compareValue = async (
+  value: string,
+  hashedValue: string,
+): Promise<boolean> => {
+  return await bcrypt.compare(value, hashedValue);
+};
