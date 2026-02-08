12f8f2404f9783b624b8b53d8e0655dbd4898d6c
Prithivi <prithivi@users.noreply.github.com>
2026-02-09 01:18:35 +0545

    Add GET route for fetching all user events

--- backend/src/routes/event.route.ts
+++ backend/src/routes/event.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createEventController } from "../controllers/event.controller.js";
+import {
+  createEventController,
+  getUserEventsController,
+} from "../controllers/event.controller.js";
 import { passportAuthenticateJwt } from "../config/passport.config.js";
 
 const eventRouter: Router = Router();
 
 eventRouter.post("/", passportAuthenticateJwt, createEventController);
+
+eventRouter.get("/all", passportAuthenticateJwt, getUserEventsController);
 
 export default eventRouter;
