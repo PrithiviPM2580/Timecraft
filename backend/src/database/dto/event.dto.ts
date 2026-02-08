b03674f240aab7a5ec04003534077c4f72ccd197
Prithivi <prithivi@users.noreply.github.com>
2026-02-09 00:40:29 +0545

    Add CreateEventDto for event creation validation

---
+++ backend/src/database/dto/event.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from "class-validator";
+import { EventLocationEnumType } from "../entities/event.entity.js";
+
+export class CreateEventDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  duration: number;
+
+  @IsEnum(EventLocationEnumType)
+  @IsNotEmpty()
+  locationType: EventLocationEnumType;
+}
