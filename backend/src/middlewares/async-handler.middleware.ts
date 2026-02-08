01b9f685418818b9c32eafb547783875527f878d
Prithivi <prithivi@users.noreply.github.com>
2026-02-08 20:42:29 +0545

    Refactor asyncHandler middleware to improve type definitions

--- backend/src/middlewares/async-handler.middleware.ts
+++ backend/src/middlewares/async-handler.middleware.ts
@@ -1,12 +1,12 @@
-import type { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction, RequestHandler } from "express";
 
 type AsyncHandlerType = (
   req: Request,
   res: Response,
   next: NextFunction,
-) => Promise<void>;
+) => Promise<unknown> | unknown;
 
-export const asyncHandler = (controller: AsyncHandlerType) => {
+export const asyncHandler = (controller: AsyncHandlerType): RequestHandler => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await controller(req, res, next);
