0a7abcdff7502993afb679b461a92e19e96fa899
Prithivi <prithivi@users.noreply.github.com>
2026-02-08 22:30:36 +0545

    Add login controller and update imports in auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,10 +1,11 @@
-import type { Request, RequestHandler, Response } from "express";
-import { RegisterDto } from "../database/dto/auth.dto.js";
+import type { Request, Response } from "express";
+import { LoginDto, RegisterDto } from "../database/dto/auth.dto.js";
 import { type Controller } from "../@types/index.js";
 import { asyncHandlerWithValidate } from "../middlewares/with-validation.middleware.js";
-import { registerService } from "../services/auth.service.js";
+import { registerService, loginService } from "../services/auth.service.js";
 import { HTTP_STATUS } from "../config/http.config.js";
 
+// Register Controller
 export const registerController: Controller = asyncHandlerWithValidate(
   RegisterDto,
   "body",
@@ -13,6 +14,20 @@ export const registerController: Controller = asyncHandlerWithValidate(
 
     return res.status(HTTP_STATUS.CREATED).json({
       message: "User registered successfully",
+      user,
+    });
+  },
+);
+
+// Login Controller
+export const loginController: Controller = asyncHandlerWithValidate(
+  LoginDto,
+  "body",
+  async (req: Request, res: Response, loginDto) => {
+    const { user } = await loginService(loginDto);
+
+    return res.status(HTTP_STATUS.OK).json({
+      message: "User logged in successfully",
       user,
     });
   },
