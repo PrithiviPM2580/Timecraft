fd8bc82fc976f401adffe7ebfa781a87a267e02a
Prithivi <prithivi@users.noreply.github.com>
2026-02-09 01:25:31 +0545

    Add PUT route for toggling event privacy with authentication

--- backend/src/routes/event.route.ts
+++ backend/src/routes/event.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   createEventController,
   getUserEventsController,
+  toggleEventPrivacyController,
 } from "../controllers/event.controller.js";
 import { passportAuthenticateJwt } from "../config/passport.config.js";
 
@@ -10,5 +11,11 @@ const eventRouter: Router = Router();
 eventRouter.post("/", passportAuthenticateJwt, createEventController);
 
 eventRouter.get("/all", passportAuthenticateJwt, getUserEventsController);
+
+eventRouter.put(
+  "/toggle-privacy",
+  passportAuthenticateJwt,
+  toggleEventPrivacyController,
+);
 
 export default eventRouter;
