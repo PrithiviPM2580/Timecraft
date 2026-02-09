a38744de99dfea9d2bdf45779c2896454e41125e
Prithivi <prithivi@users.noreply.github.com>
2026-02-09 20:25:19 +0545

    Implement update user availability service with DTO mapping and save logic

--- backend/src/services/availability.service.ts
+++ backend/src/services/availability.service.ts
@@ -3,6 +3,9 @@ import { User } from "../database/entities/user.entity.js";
 import type { AvailabilityResponseType } from "../@types/index.js";
 import { NotFoundException } from "../utils/app-error.js";
 import logger from "../utils/logger.js";
+import { UpdateAvailabilityDto } from "../database/dto/availability.dto.js";
+import { Availability } from "../database/entities/availability.entity.js";
+import type { DayOfWeekEnum } from "../database/entities/day-availability.entity.js";
 
 export const getUserAvailabilityService = async (userId: string) => {
   const userRepository = AppDataSource.getRepository(User);
@@ -36,4 +39,51 @@ export const getUserAvailabilityService = async (userId: string) => {
   });
 
   return availabilityData;
+};
+
+export const updateUserAvailabilityService = async (
+  userId: string,
+  updateAvailabilityDto: UpdateAvailabilityDto,
+) => {
+  const userRepository = AppDataSource.getRepository(User);
+  const availabilityRepository = AppDataSource.getRepository(Availability);
+
+  const user = await userRepository.findOne({
+    where: { id: userId },
+    relations: ["availability", "availability.days"],
+  });
+
+  if (!user) {
+    logger.error(`User with id ${userId} not found`, {
+      label: "AvailabilityService",
+    });
+    throw new NotFoundException("User not found");
+  }
+
+  const dayAvailabilityData = updateAvailabilityDto.days.map(
+    ({ day, isAvailable, startTime, endTime }) => {
+      const baseDate = new Date().toISOString().split("T")[0];
+      return {
+        day: day.toUpperCase() as DayOfWeekEnum,
+        startTime: new Date(`${baseDate}T${startTime}:00Z`),
+        endTime: new Date(`${baseDate}T${endTime}:00Z`),
+        isAvailable,
+      };
+    },
+  );
+
+  if (user.availability) {
+    await availabilityRepository.save({
+      id: user.availability.id,
+      timeGap: updateAvailabilityDto.timeGap,
+      days: dayAvailabilityData.map((day) => ({
+        ...day,
+        availability: { id: user.availability?.id },
+      })),
+    });
+  }
+
+  return {
+    success: true,
+  };
 };
