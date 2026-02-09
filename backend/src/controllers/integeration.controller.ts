8ca21e8fb11ba098b0bd9be71ffe824827df0df0
Prithivi <prithivi@users.noreply.github.com>
2026-02-09 23:30:44 +0545

    Add checkUserIntegerationController with validation for app type

--- backend/src/controllers/integeration.controller.ts
+++ backend/src/controllers/integeration.controller.ts
@@ -4,7 +4,12 @@ import { asyncHandler } from "../middlewares/async-handler.middleware.js";
 import { HTTP_STATUS } from "../config/http.config.js";
 import logger from "../utils/logger.js";
 import { NotFoundException } from "../utils/app-error.js";
-import { getUserIntegerationsService } from "../services/integeration.service.js";
+import {
+  getUserIntegerationsService,
+  checkUserIntegerationService,
+} from "../services/integeration.service.js";
+import { asyncHandlerWithValidate } from "../middlewares/with-validation.middleware.js";
+import { AppTypeDto } from "../database/dto/integeration.dto.js";
 
 export const getUserIntegerationController: Controller = asyncHandler(
   async (req: Request, res: Response) => {
@@ -23,3 +28,27 @@ export const getUserIntegerationController: Controller = asyncHandler(
     });
   },
 );
+
+export const checkUserIntegerationController: Controller =
+  asyncHandlerWithValidate(
+    AppTypeDto,
+    "params",
+    async (req: Request, res: Response, appTypeDto) => {
+      const userId = req.user?.id;
+
+      if (!userId) {
+        logger.error("User ID is missing in the request");
+        throw new NotFoundException("User not found");
+      }
+
+      const isConnected = await checkUserIntegerationService(
+        userId,
+        appTypeDto.appType,
+      );
+
+      return res.status(HTTP_STATUS.OK).json({
+        message: "Integeration checked successfully",
+        isConnected,
+      });
+    },
+  );
