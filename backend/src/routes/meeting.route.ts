275fcf6410fa0dc76a056fa8c3acc4628bc33a94
Prithivi <prithivi@users.noreply.github.com>
2026-02-10 23:51:31 +0545

    Add createMeetingBookingForGuestController to meeting routes

--- backend/src/routes/meeting.route.ts
+++ backend/src/routes/meeting.route.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { passportAuthenticateJwt } from "../config/passport.config.js";
-import { getUserMeetingsController } from "../controllers/meeting.controller.js";
+import {
+  createMeetingBookingForGuestController,
+  getUserMeetingsController,
+} from "../controllers/meeting.controller.js";
 
 const meetingRouter: Router = Router();
 
@@ -9,5 +12,7 @@ meetingRouter.get(
   passportAuthenticateJwt,
   getUserMeetingsController,
 );
+
+meetingRouter.post("/public/create", createMeetingBookingForGuestController);
 
 export default meetingRouter;
